60f25e9c3617f28936d92661eec49d42f3aff78f
hcz017 <hcz017@gmail.com>
2020-09-23 16:58:33 +0800

    update script, fix list content error

--- datasets/prepare_file_and_labels_list.sh
+++ datasets/prepare_file_and_labels_list.sh
@@ -9,7 +9,7 @@ grep -rnE [8-9].jpg fake_all_list.txt | cut -d : -f 2- > fake_data_test_list.txt
 grep -rnE [0-3].jpg fake_all_list.txt | cut -d : -f 2- > fake_data_train_list.txt 
 grep -rnE [4-7].jpg fake_all_list.txt | cut -d : -f 2- > fake_data_val_list.txt 
 
-ls ../datasets/fake/* | cut -d "/" -f 2- > real_all_list.txt
+ls ../datasets/real/* | cut -d "/" -f 2- > real_all_list.txt
 grep -rnE [8-9].jpg real_all_list.txt | cut -d : -f 2- > real_data_test_list.txt
 grep -rnE [0-3].jpg real_all_list.txt | cut -d : -f 2- > real_data_train_list.txt 
 grep -rnE [4-7].jpg real_all_list.txt | cut -d : -f 2- > real_data_val_list.txt 
