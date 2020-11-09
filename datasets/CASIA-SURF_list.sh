f1537fa2a078bce49ba4bb5993c8bd3b543c9b84
hcz017 <hcz017@gmail.com>
2020-11-09 16:59:28 +0800

    enlarge training datasets

--- datasets/CASIA-SURF_list.sh
+++ datasets/CASIA-SURF_list.sh
@@ -3,32 +3,38 @@ rm -rf real_*
 rm -rf fake_*
 
 echo "prepare list"
-grep -rnE "real.*depth" train_list.txt |cut -d " " -f 2 > real_train_depth_list.txt
-grep -rnE "fake.*depth" train_list.txt |cut -d " " -f 2 > fake_train_depth_list.txt
-
-sed -n '1,800p' real_train_depth_list.txt > real_data_train_list.txt 
-sed -n '1001,1800p' real_train_depth_list.txt > real_data_val_list.txt 
-sed -n '2001,2400p' real_train_depth_list.txt > real_data_test_list.txt
-
-sed -n '1,800p' fake_train_depth_list.txt > fake_data_train_list.txt 
-sed -n '1001,1800p' fake_train_depth_list.txt > fake_data_val_list.txt 
-sed -n '2001,2400p' fake_train_depth_list.txt > fake_data_test_list.txt
-
+# 8000 8000 4000
+grep -rnE "fake.*depth" train_list.txt |cut -d " " -f 2 > _fake_train_depth_list.txt
+sed -n '1,8000p' _fake_train_depth_list.txt > fake_data_train_list.txt 
+sed -n '8001,16000p' _fake_train_depth_list.txt > fake_data_val_list.txt 
+sed -n '16001,20000p' _fake_train_depth_list.txt > fake_data_test_list.txt
+# 3200 3200 1600
+grep -rnE "real.*depth" train_list.txt |cut -d " " -f 2 > _real_train_depth_list.txt
+sed -n '1,3200p' _real_train_depth_list.txt > real_data_train_list.txt 
+sed -n '3201,6400p' _real_train_depth_list.txt > real_data_val_list.txt 
+sed -n '6401,8000p' _real_train_depth_list.txt > real_data_test_list.txt
 
 echo "prepare labels"
 cnt=1
-while(( $cnt<=800 ))
+while(( $cnt<=8000 ))
 do
-    let "cnt++"
-    echo "1">> real_labels_train_list.txt
-    echo "1">> real_labels_val_list.txt
     echo "0">> fake_labels_train_list.txt
     echo "0">> fake_labels_val_list.txt
-done
-cnt=1
-while(( $cnt<=400 ))
-do
+    if (($cnt <=4000 ))
+    then
+        echo "0">> fake_labels_test_list.txt
+    fi
     let "cnt++"
-    echo "1">> real_labels_test_list.txt
-    echo "0">> fake_labels_test_list.txt
+done
+
+cnt=1
+while(( $cnt<=3200 ))
+do
+    echo "1">> real_labels_train_list.txt
+    echo "1">> real_labels_val_list.txt
+    if (($cnt <=1600 ))
+    then
+        echo "1">> real_labels_test_list.txt
+    fi
+    let "cnt++"
 done
